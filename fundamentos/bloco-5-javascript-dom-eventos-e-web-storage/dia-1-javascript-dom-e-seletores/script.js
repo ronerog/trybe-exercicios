012345afc8ada75108ae37512c9ced81b9e0240a
ronerog <ronerojr@gmail.com>
2022-05-19 18:48:41 -0300

    exercicios dia 5.1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,8 @@
+
+let cor = document.getElementById("header-container")
+cor.style.color = 'red'
+
+document.querySelector(".emergency-tasks h3").innerText = 'XABLAU'
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].innerText = 'ACORDA!'
+
