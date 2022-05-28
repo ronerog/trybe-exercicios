6420394e32edc4186a4a59cd6e21fb6655fe6a40
ronerog <ronerojr@gmail.com>
2022-05-28 13:45:27 -0300

    exercicio3

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -26,4 +26,14 @@ bis.innerText = 'Sou o bisneto'
 
 console.log(bis.parentNode.parentNode.nextElementSibling)
 
-//REMOVEa
+const pai = document.getElementById('pai');
+
+    for (let index = pai.childNodes.length - 1; index >= 0; index -= 1) {
+      const currentChildren = pai.childNodes[index];
+      if (currentChildren.id !== 'elementoOndeVoceEsta') {
+        currentChildren.remove();
+      }
+    }
+
+    const segundoEUltimoFilhoDoFilho = document.getElementById('segundoEUltimoFilhoDoFilho');
+    segundoEUltimoFilhoDoFilho.remove();
